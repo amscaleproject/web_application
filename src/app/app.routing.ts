7eaa8b57aeaf108e7fa06fcc6dbf20bda643a82b
Ekaterina Mosseyko <kat@tmk-serv.skz-serv>
2023-05-07 00:35:31 +0300

    Target routing paths

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -19,4 +19,30 @@ const appRoutes: Routes = [
     { path: '**', redirectTo: '' }
 ];
 
+/*
+const appRoutes: Routes = [
+    {path:"license",component:wc},
+    {path:"login",component:fg,canActivate:[jB],data:GB},
+    {path:"home",component:tI,canActivate:[jB],data:zB},
+    {path:"activity",component:gS,canActivate:[jB],data:KB},
+    {path:"alerts",component:oA,canActivate:[jB],data:$B},
+    {path:"sds/create",component:Rx,canActivate:[jB],data:ZB},
+    {path:"sds/create/pool/:poolId",component:Rx,canActivate:[jB],data:UB},
+    {path:"pool/create",component:nM,canActivate:[jB],data:XB},
+    {path:"pool/list",component:iO,canActivate:[jB],data:JB},
+    {path:"sds/list",component:xO,canActivate:[jB],data:qB},
+    {path:"node/list",component:QO,canActivate:[jB],data:QB},
+    {path:"sds/edit/:uuid",component:MF,canActivate:[jB],data:eY},
+    {path:"sds/edit/advanced/:uuid",component:bD,canActivate:[jB],data:tY},
+    {path:"pool/edit/:uuid",component:CD,canActivate:[jB],data:nY},
+    {path:"pool/:uuid",component:xD,canActivate:[jB],data:iY},
+    {path:"sds/:uuid",component:sP,canActivate:[jB],data:lY},
+    {path:"node/:uuid",component:lE,canActivate:[jB],data:oY},
+
+    {path:"",redirectTo:"/home",pathMatch:"full"},
+
+    {path:"**",component:OE}
+];
+*/
+
 export const routing = RouterModule.forRoot(appRoutes);
