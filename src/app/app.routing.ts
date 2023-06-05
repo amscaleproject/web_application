39dee00ed649cbc65da659bb9f3c0ccffc574f1a
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2023-06-05 15:02:46 +0700

    add pool list route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { TestwsComponent } from './testws';
+import {PoolListComponent} from "./pool-list/pool-list.component";
 import { AuthGuard } from './_guards';
 
 const appRoutes: Routes = [
@@ -11,6 +12,7 @@ const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'testws', component: TestwsComponent },
+    { path: 'pool/list', component: PoolListComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
