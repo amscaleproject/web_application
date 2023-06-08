1e1bad96a28f667f866cab6d06a607b1f84ae938
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2023-06-08 19:07:56 +0700

    add test hide element

--- src/app/_services/Overlay/overlay.service.ts
+++ src/app/_services/Overlay/overlay.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { DetailOverlayComponent } from "../../detail-overlay/detail-overlay.component";
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Injectable({
   providedIn: 'root'
@@ -16,13 +17,19 @@ export class OverlayService {
     this.overlayRef = this.overlay.create(this.getOverlayConfig());
     const componentRef = this.overlayRef.attach(
         new ComponentPortal(DetailOverlayComponent,
-        null,
-        this.createInjector(content))
+            null,
+            this.createInjector(content))
     );
   }
   closeOverlay() {
     if (this.overlayRef) {
-      this.overlayRef.detach();
+      const animation = trigger('fadeOut', [
+        transition(':leave', [
+          animate('0.3s', style({ opacity: 0 }))
+        ])
+      ]);
+      const component = this.overlayRef.detach();
+      component.instance.overlayAnimation = animation;
       this.overlayRef = null;
     }
   }
@@ -31,9 +38,9 @@ export class OverlayService {
       hasBackdrop: true,
       backdropClass: 'cdk-overlay-transparent-backdrop',
       positionStrategy: this.overlay.position().
-        global().
-        right().
-        centerVertically(),
+      global().
+      right().
+      centerVertically(),
     });
   }
   private createInjector(content: string): Injector {
--- src/app/detail-overlay/detail-overlay.component.ts
+++ src/app/detail-overlay/detail-overlay.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, Input} from '@angular/core';
 import {OverlayService} from "../_services/Overlay/overlay.service";
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-detail-overlay',
@@ -12,18 +12,50 @@ import { trigger, transition, style, animate } from '@angular/animations';
         style({ transform: 'translateX(100%)' }),
         animate('0.3s ease-out', style({ transform: 'translateX(0%)' }))
       ])
+    ]),
+    trigger('fadeInOnEnter', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.3s ease-in', style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('hideElement', [
+      state('hidden', style({
+        opacity: 0,
+        height: 0,
+        padding: 0,
+        margin: 0,
+        display: 'none'
+      })),
+      state('visible', style({
+        opacity: 1,
+        height: '*',
+        padding: '*',
+        margin: '*',
+        display: 'block'
+      })),
+      transition('visible => hidden', [
+        animate('0.3s')
+      ]),
+      transition('hidden => visible', [
+        animate('0.3s')
+      ])
     ])
   ]
 })
 export class DetailOverlayComponent {
-
+  isHidden = false;
   constructor(
       @Inject('content') public content: string,
       public OverlayService: OverlayService
   ) {}
-
+  hideElement() {
+    this.isHidden = true;
+  }
   closeOverlay(){
     console.log('close overlay');
+
+
     this.OverlayService.closeOverlay();
   }
 
