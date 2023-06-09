c8f0c664d3496f235bf7d49e9ff85d28ff9ffdf5
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2023-06-09 10:33:37 +0700

    format home help

--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -13,18 +13,21 @@ export class PageHeaderComponent {
   @Input() title: string | undefined;
 
   contentInput = `    
-      <div _ngcontent-hoy-c13="" class="help-content">
-          <div _ngcontent-hoy-c13="" class="detail-overlay-header"><h2 _ngcontent-hoy-c13="">Dashboard Help Guide</h2>
-          </div>
-          <h3 _ngcontent-hoy-c13="">AttikaMove Dashboard</h3>
-          <p _ngcontent-hoy-c13="">The AttikaMove Dashboard provides a high level view into all the SDServers and
-              Nodes managed by this instance of the AttikaMove management software. The Dashboard view is the default
-              page on logging into AttikaMove.</p>
-          <p _ngcontent-hoy-c13=""><span _ngcontent-hoy-c13="" class="material-icons help-more-info">info</span> At
-              any time you may return to the Dashboard by clicking on the "CompanyMove" logo in the top left of the
-              browser.
-          </p>
-      </div>      
+    <div class="help-content">
+      <div class="detail-overlay-header">
+        <h2>Dashboard Help Guide</h2>
+      </div>
+      <h3>AttikaMove Dashboard</h3>
+        <p>
+          The AttikaMove Dashboard provides a high level view into all the SDServers and
+          Nodes managed by this instance of the AttikaMove management software. The Dashboard view is the default
+          page on logging into AttikaMove.
+        </p>
+      <p>
+        <span class="material-icons help-more-info">info</span> 
+        At any time you may return to the Dashboard by clicking on the "CompanyMove" logo in the top left of the browser.
+      </p>
+    </div>      
     `;
   constructor(public OverlayService: OverlayService  ) {}
 
