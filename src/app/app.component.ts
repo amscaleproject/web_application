d03b246f3310192ca717835f8c6d8aa3c585c9fb
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2023-07-01 08:36:15 +0700

    add terminal by button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {LanguageService} from "./_services/LanguageService/language-service.service";
+import { MatDialog } from "@angular/material/dialog";
+import { LanguageService } from "./_services/LanguageService/language-service.service";
+import {TerminalComponent} from "./terminal/terminal.component";
 
 
 @Component({
     selector: 'app',
     templateUrl: 'app.component.html'
 })
-
 export class AppComponent {
     currentLanguage: string;
-    constructor(private languageService: LanguageService) {
+
+    constructor(
+        private languageService: LanguageService,
+        private dialog: MatDialog
+    ) {
         this.languageService.getCurrentLanguage().subscribe(lang => {
             this.currentLanguage = lang;
         });
     }
+
     setLanguage(lang: string): void {
         this.languageService.setCurrentLanguage(lang);
         this.currentLanguage = lang;
     }
 
-
+    openTerminal(): void {
+        this.dialog.open(TerminalComponent);
+    }
 }
--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -49,4 +49,7 @@ export class TerminalComponent implements AfterViewInit {
       }
     });
   }
+
+
+
 }
